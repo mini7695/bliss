b61c8090b791475d889523275dbc4635784a0b19
Minithasri k <113795715+mini7695@users.noreply.github.com>
2024-10-01 19:19:43 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        color: "#F9AE8F",
+      },
+      fontFamily: {
+        head1: ["Grey Qo"],
+      },
+    },
+  },
+  plugins: [],
+};
